e5c4f1e1dce2abc6838e8e349ce6e39537994ae5
Semen Fomchenkov <armatik@altlinux.org>
2026-03-10 21:00:17 +0300

    feat: add tag badge overlay on release cards

--- src/kitsune/ui/widgets/release_card.py
+++ src/kitsune/ui/widgets/release_card.py
@@ -7,9 +7,10 @@ import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 
-from gi.repository import Adw, Gtk
+from gi.repository import Adw, Gdk, Gtk
 
 from kitsune.models import Release
+from kitsune import tags_store
 from kitsune.ui.image_cache import load_image
 
 
@@ -22,6 +23,9 @@ class ReleaseCard(Gtk.FlowBoxChild):
     spinner = Gtk.Template.Child()
     title_label = Gtk.Template.Child()
     subtitle_label = Gtk.Template.Child()
+    tag_badges = Gtk.Template.Child()
+
+    _badge_css_loaded = False
 
     def __init__(self, release: Release, **kwargs):
         super().__init__(**kwargs)
@@ -47,6 +51,75 @@ class ReleaseCard(Gtk.FlowBoxChild):
         else:
             self.spinner.set_visible(False)
             self.placeholder.set_visible(True)
+
+        self._populate_tag_badges()
+
+    @classmethod
+    def _ensure_badge_css(cls):
+        if cls._badge_css_loaded:
+            return
+        cls._badge_css_loaded = True
+        css = Gtk.CssProvider()
+        css.load_from_string(
+            '.tag-badge { min-width: 24px; min-height: 24px;'
+            ' border-radius: 50%; background: alpha(black, 0.6);'
+            ' border: 1.5px solid alpha(white, 0.12); }'
+            ' .tag-badge-plus { background: alpha(@accent_bg_color, 0.3);'
+            ' border-color: alpha(@accent_bg_color, 0.5); }'
+            ' .tag-badge-emoji { font-size: 13px; }'
+            ' .tag-badge-color { min-width: 14px; min-height: 14px;'
+            ' border-radius: 50%; }'
+        )
+        Gtk.StyleContext.add_provider_for_display(
+            Gdk.Display.get_default(), css,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
+        )
+
+    def _populate_tag_badges(self):
+        self._ensure_badge_css()
+        tags = tags_store.get_tags_for_release(self.release.id)
+        if not tags:
+            return
+
+        self.tag_badges.set_visible(True)
+        max_visible = 3
+        visible_tags = tags[:max_visible]
+        has_more = len(tags) > max_visible
+
+        for i, tag in enumerate(visible_tags):
+            badge = Gtk.Box(
+                halign=Gtk.Align.CENTER, valign=Gtk.Align.CENTER,
+                css_classes=['tag-badge'],
+            )
+            if i > 0:
+                badge.set_margin_start(-6)
+            if tag['icon_type'] == 'emoji':
+                badge.append(Gtk.Label(
+                    label=tag['icon_value'],
+                    css_classes=['tag-badge-emoji'],
+                ))
+            else:
+                from kitsune.ui.widgets.tag_card import COLOR_MAP
+                hex_c = COLOR_MAP.get(tag['icon_value'], '#6e7781')
+                circle = Gtk.Box(css_classes=['tag-badge-color'])
+                c_css = Gtk.CssProvider()
+                c_css.load_from_string(
+                    f'.tag-badge-color {{ background: {hex_c}; }}'
+                )
+                circle.get_style_context().add_provider(
+                    c_css, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
+                )
+                badge.append(circle)
+            self.tag_badges.append(badge)
+
+        if has_more:
+            plus = Gtk.Box(
+                halign=Gtk.Align.CENTER, valign=Gtk.Align.CENTER,
+                css_classes=['tag-badge', 'tag-badge-plus'],
+                margin_start=-6,
+            )
+            plus.append(Gtk.Label(label='+', css_classes=['accent']))
+            self.tag_badges.append(plus)
 
     def _on_preview_loaded(self, texture, error):
         if texture and not self.picture.get_paintable():
